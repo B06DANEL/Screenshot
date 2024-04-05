c264658b22f68806a6585361b2fea2e7300663fb
B06DANEL <74467749+B06DANEL@users.noreply.github.com>
2024-04-05 16:43:21 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    Screenshot.Invert()
-})
-basic.showLeds(`
-    . # . # .
-    # # # # #
-    # # # # #
-    . # # # .
-    . . # . .
-    `)
-Screenshot.Save()
-Screenshot.SetUOEC(true)
-loops.everyInterval(500, function () {
-    Screenshot.Rotate(false)
-})
+
