7274bc1efd223dcff563c432c753581e1adbaa76
B06DAN3L2 <ionitabogdanmihai2013@gmail.com>
2024-04-21 20:55:41 +0300

    Now you can name you screenshots. Added a new block: "Prepare screenshot for modifying"

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Create, manage and load screenshots. This extension was made for Makecode for Mi
 
 ## Blocks
 
- •**Save screenshot**: save the Microbit's screen's current state.
+ •**Save screenshot**: save an namebale screenshot of Microbit's screen's current state.
  
- •**Load screenshot**: load the saved screenshot.
+ •**Load screenshot**: load the saved screenshot with the chosen name.
  
- •**Invert screenshot**: Inverts the screenshot
+ •**Invert screenshot**: Inverts the last screenshot saved or loaded
  
- •**Rotate screenshot**: Rotates the screenshot clockwise/counterclockwise.
+ •**Rotate screenshot**: Rotates the last screenshot saved or loaded clockwise/counterclockwise.
+ 
+ •**Update on every change**: Toggles if the screen is updated on every change (when an screenshot is modified by "Invert Screenshot" and "Rotate Screenshot").
+
+ •**Prepared screenshot for modifying**: Makes it so when it inverts or rotates an screenshot instead of using the last Screenshot saved or loaded it uses an specific screenshot
+
 
--- Screenshot.ts
+++ Screenshot.ts
@@ -2,23 +2,32 @@
 namespace screenshot {
     //Makes an screenshot of the screen, Modifies it and Loads it
     let screen = [false]
+    let screenshots = [[false]]
+    let screenshotname = ["Dummy"]
     //UOEC = Update On Every Change
     let UOECb = false
+    let lastscreenshot = ""
+
     function floor(num: number) {
         return num - (num % 1)
     }
     //% blockId=saveScreenshot
-    //% block="save screenshot"
-    export function save() {
+    //% block="save screenshot named $name"
+    export function save(name: string) {
         screen = []
+        lastscreenshot = name
         for (let i = 0; i <= 24; i++){
             screen.push (led.point(i % 5, floor(i / 5)))
         }
+        screenshots.push(screen)
+        screenshotname.push(name)
     }
     //% blockId=LoadScreenshot
-    //% block="load screenshot"
-    export function load() {
+    //% block="load screenshot named $name"
+    export function load(name: string) {
         basic.clearScreen()
+        lastscreenshot = name
+        screen = screenshots[screenshotname.indexOf(name)]
         for (let i = 0; i <= 24; i++) {
             if (screen[i] != false) {
                 led.plot(i % 5, floor(i / 5))
@@ -28,17 +37,20 @@ namespace screenshot {
     //% blockId=Invert
     //% block="invert screenshot"
     export function invert() {
+        screen = screenshots[screenshotname.indexOf(lastscreenshot)]
         for (let i = 0; i <= 24; i++) {
             screen[i] = !screen[i]
         }
+        screenshots[screenshotname.indexOf(lastscreenshot)] = screen
         if (UOECb) {
-            load()
+            load(lastscreenshot)
         }
     }
     //% blockId=Rotate
     //% block="rotate screenshot: counterclockwise $c"
     //% c.defl=false
     export function rotate(c: boolean) {
+        screen = screenshots[screenshotname.indexOf(lastscreenshot)]
         let rotindex = [5, 10, 15, 20, 25, 4, 9, 14, 19, 24, 3, 8, 13, 18, 23, 2, 7, 12, 17, 22, 1, 6, 11, 16, 21]
         let temp = screen
         let temp2 = [false]; temp2 = []
@@ -47,8 +59,9 @@ namespace screenshot {
                 temp2.push (temp[rotindex[i]-1])
             }
             screen = temp2
+            screenshots[screenshotname.indexOf(lastscreenshot)] = screen
             if (UOECb) {
-                load()
+                load(lastscreenshot)
             }
         } else {
             rotate (true)
@@ -59,7 +72,16 @@ namespace screenshot {
     //% blockId=SetUOEC
     //% block="update on every change $c"
     //% c.defl=true
+    //%advanced=true
     export function setUOEC (c: boolean) {
         UOECb = c
+    }
+
+    //% blockId=PrepareScreenshot
+    //% block="prepare screenshot named $c for modifying"
+    //% c.defl=true
+    //%advanced=true
+    export function prepareScreenshot(c: string) {
+        lastscreenshot = c
     }
 }
